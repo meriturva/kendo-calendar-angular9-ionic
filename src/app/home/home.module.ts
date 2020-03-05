ae19cca78d7762edf77f3d6360e1024c53c140a0
Diego Bonura <diego@bonura.dev>
2020-03-05 15:55:59 +0100

    First commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,11 +6,14 @@ import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    DateInputsModule,
     RouterModule.forChild([
       {
         path: '',
